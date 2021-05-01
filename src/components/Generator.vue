3f4c2575e26b78152ed8698d6e499332dbda9d43
chinemelu <chinemelunwosu@gmail.com>
2021-05-01 23:56:27 +0100

    update template of project generator

--- src/components/Generator.vue
+++ src/components/Generator.vue
@@ -1,8 +1,25 @@
 <template>
-  <div>
-
-  </div>
+<div class="generator">
+  <section class="hero is-primary is-bold has-text-centered py-6">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title">
+          What skills do you want to work on?
+        </h1>
+        <div v-for="skill in skillList" :key="skill.id">
+          <div class="field">
+            <label class="checkbox">
+              <input type="checkbox" :value="skill.id">
+              {{ skill.skill }}
+            </label>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</div>
 </template>
+
 
 <script>
   import { ref } from 'vue';
