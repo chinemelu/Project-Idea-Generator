0b04f382f7fd115a2b1d8c4171c8856151e937e1
chinemelu.nwosu <chinemelu.nwosu@brandmobile.net>
2021-05-01 23:43:49 +0100

    get first taste of creating composition API

--- src/components/Generator.vue
+++ src/components/Generator.vue
@@ -5,8 +5,24 @@
 </template>
 
 <script>
+  import { ref } from 'vue';
   export default {
-    name: 'Generator'
+    name: 'Generator',
+    setup() {
+      const GENERATOR_BASE = 'http://localhost:3000';
+      const skillList = ref([]);
+
+      async function getSkillList() {
+        const response = await fetch(`${GENERATOR_BASE}/skills`);
+        skillList.value = await response.json();
+      }
+
+      getSkillList();
+
+      return {
+        skillList,
+      }
+    }
   }
 </script>
 
